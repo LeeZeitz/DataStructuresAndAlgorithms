da506c0103d13a1ff4df54781d90a2f4f132b6a5
LeeZeitz <andrew.l.zeitz@gmail.com>
2020-03-22 16:18:56 -0700

    add linked list interface

---
+++ src/linked_list/linked_list.py
@@ -0,0 +1,23 @@
+
+# TODO: implement pls
+class LinkedList:
+
+    def insert(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def contains(self):
+        pass
+
+    def length(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def pop(self):
+        pass
+
+    
